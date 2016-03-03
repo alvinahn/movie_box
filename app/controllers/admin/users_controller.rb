c5b5059da37c5aa523ab5bebd3c73a0dee140a4d
Alvin <snowmt7@msn.com>
2016-03-03 07:17:31 +0000

    added bonus features

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -5,4 +5,43 @@ class Admin::UsersController < ApplicationController
     @users = User.all.page(params[:page]).per(10)
   end
 
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+
+    if @user.save
+      redirect_to admin_users_path, notice: "You just created #{@user.full_name}!"
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @user = User.find(params[:id])    
+  end
+
+  def update
+    @user = User.find(params[:id])
+
+    if @user.update_attributes(user_params)
+      redirect_to admin_users_path
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @user = User.find(params[:id])
+    @user.destroy
+    redirect_to admin_users_path
+  end
+
+  protected
+  def user_params
+    params.require(:user).permit(:email, :firstname, :lastname, :password, :password_confirmation, :admin)
+  end
+
 end
